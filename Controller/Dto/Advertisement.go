bf8ef85e4f24207d3b4126be64a69059d3e8a9ab
Revelt <revelt@lc-rp.mp>
2022-07-03 20:27:10 +0700

    🔨feat(app): modify advertisement struct and ad resp of buying ads

--- Controller/Dto/Advertisement.go
+++ Controller/Dto/Advertisement.go
@@ -1,26 +1,29 @@
 package Dto
 
-type CreateAdvertisement struct {
-	Name      string  `json:"name"`
-	BasePrice float64 `json:"base_price"`
-	Category  int     `json:"category"`
-	CreatedBy int     `json:"created_by"`
-	CreatedAt string  `json:"created_at"`
-	UpdatedAt string  `json:"updated_at"`
+type SuccessCreateAdvertisement struct {
+	Message               string                  `json:"message"`
+	UpdatedCompanyBalance UpdatedCompanyBalance   `json:"company_info"`
+	CreateAdvertisement   AdvertisementAttributes `json:"created_advertisement"`
 }
 
-type SuccessCreateAdvertisement struct {
-	Message               string                `json:"message"`
-	UpdatedCompanyBalance UpdatedCompanyBalance `json:"company_info"`
-	CreateAdvertisement   CreateAdvertisement   `json:"created_advertisement"`
+type SuccessBuyAdvertisement struct {
+	Message           string                  `json:"message"`
+	CompanyInfo       UpdatedCompanyBalance   `json:"company_info"`
+	AdvertisementInfo AdvertisementAttributes `json:"advertisement_info"`
+	//TransactionInfo   Models.Transaction      `json:"transaction_info"`
 }
 
 type BuyAdvertisement struct {
-	Id        int `json:"id"`
-	CompanyId int `json:"company_id"`
+	AdvertisementId int `json:"advertisement_id"`
+	CompanyId       int `json:"company_id"`
 }
 
-type ViewAllAdvertisements struct {
+type RespBuyAdvertisement struct {
+	Id      int    `json:"advertisement_id"`
+	Message string `json:"message"`
+}
+
+type AdvertisementAttributes struct {
 	Id        int     `json:"id"`
 	Name      string  `json:"name"`
 	BasePrice float64 `json:"base_price"`
